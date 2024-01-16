4f0f02580125e7cad16f2fdc4a1a43d1c391e2fe
Ebuube <onwutaebubegideon1555@gmail.com>
2024-01-16 14:41:00 +0000

    Task 11: Made code pycodestyle compliant

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -5,7 +5,10 @@ import typing
 
 T = typing.TypeVar('T')
 
-def safely_get_value(dct: typing.Mapping, key: typing.Any, default: typing.Union[T, None] = None) -> typing.Union[typing.Any, T]:
+
+def safely_get_value(dct: typing.Mapping, key: typing.Any,
+                     default: typing.Union[T, None] = None) -> \
+                     typing.Union[typing.Any, T]:
     '''Safely get a value from a mapping if it exists'''
     if key in dct:
         return dct[key]
