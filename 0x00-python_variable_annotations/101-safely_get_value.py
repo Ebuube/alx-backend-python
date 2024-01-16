afa8a06e6db3fd67808da2daf1ac31eb91e996a5
Ebube Gideon Onwuta <onwutaebubegideon1555@gmail.com>
2024-01-16 15:35:46 +0100

    Added generic type

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -3,7 +3,9 @@
 import typing
 
 
-def safely_get_value(dct: typing.Mapping, key: typing.Any, default: typing.Optional[typing.Any] = None) -> typing.Optional[typing.Any]:
+T = typing.TypeVar('T')
+
+def safely_get_value(dct: typing.Mapping, key: typing.Any, default: typing.Union[T, None] = None) -> typing.Union[typing.Any, T]:
     '''Safely get a value from a mapping if it exists'''
     if key in dct:
         return dct[key]
