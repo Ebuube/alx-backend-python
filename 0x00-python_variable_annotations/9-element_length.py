a39d8ae140aa650e0210426a34ddbea06f59ea31
Ebuube <onwutaebubegideon1555@gmail.com>
2024-01-16 03:13:40 +0000

    Pycodestyle compliant: made files executable

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -3,6 +3,7 @@
 import typing
 
 
-def element_length(lst: typing.Iterable[typing.Sequence]) -> typing.List[typing.Tuple[typing.Sequence, int]]:
+def element_length(lst: typing.Iterable[typing.Sequence]) \
+        -> typing.List[typing.Tuple[typing.Sequence, int]]:
     '''Return each iterable as well as its length in a list of tuples'''
     return [(i, len(i)) for i in lst]
