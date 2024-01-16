1b9397019852609d68661177ddd86215c90d46fc
Ebube Gideon Onwuta <onwutaebubegideon1555@gmail.com>
2024-01-16 04:10:42 +0100

    Updated: Added docstring

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -4,5 +4,5 @@ import typing
 
 
 def element_length(lst: typing.Iterable[typing.Sequence]) -> typing.List[typing.Tuple[typing.Sequence, int]]:
-    ''''''
+    '''Return each iterable as well as its length in a list of tuples'''
     return [(i, len(i)) for i in lst]
