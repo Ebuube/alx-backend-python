e42246622e62a5dce71e276c08c384d47ab93908
Ebube Gideon Onwuta <onwutaebubegideon1555@gmail.com>
2024-01-16 03:25:18 +0100

    Task 1: Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+This module contains the definition of type-annotated function
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    '''Return a concatenated string'''
+    return str1 + str2
