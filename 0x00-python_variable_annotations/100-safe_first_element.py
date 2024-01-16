f2fbb5f756f05566e73b120eca8e3558d23d12d0
Ebube Gideon Onwuta <onwutaebubegideon1555@gmail.com>
2024-01-16 04:18:58 +0100

    Task 10: Duck typing - first element of a sequence

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""This module contains a type-annotated function"""
+import typing
+
+
+def safe_first_element(lst: typing.Sequence[typing.Any]) -> \
+    typing.Union[typing.Any, None]:
+    '''Return the first element of the sequence if any'''
+    if lst:
+        return lst[0]
+    else:
+        return None
