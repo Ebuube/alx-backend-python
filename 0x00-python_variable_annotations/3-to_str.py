a452e5718752315dbbe6c133c569fc75d447351e
Ebube Gideon Onwuta <onwutaebubegideon1555@gmail.com>
2024-01-16 03:38:37 +0100

    Type 3: Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module contains a type-annotated function"""
+
+
+def to_str(n: float) -> str:
+    """Return a string representation of the argument"""
+    return str(n)
