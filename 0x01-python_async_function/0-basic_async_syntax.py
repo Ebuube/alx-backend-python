f6775464b69a9242003631393284d26074e0d7f1
Ebube Gideon Onwuta <onwutaebubegideon1555@gmail.com>
2024-01-16 18:12:29 +0100

    Modified waiting period

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,6 +6,6 @@ import random
 
 async def wait_random(max_delay: int = 10) -> float:
     """"Wait for a random period"""
-    pause = random.randint(0, max_delay)
+    pause = (random.random() * 10) % max_delay
     asyncio.sleep(pause)
-    return pause
+    return float(pause)
