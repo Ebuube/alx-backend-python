0bb4c81118db83a260c05a66d9128e15a11f0cbb
Ebuube <onwutaebubegideon1555@gmail.com>
2024-01-16 14:43:15 +0000

    Task 12: docstring to zoom_array function

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -6,6 +6,9 @@ from typing import Tuple, List
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    '''
+    Return a zoomed version of a list
+    '''
     zoomed_in: List = [
             item for item in lst
             for i in range(factor)
