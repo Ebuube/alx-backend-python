3a9682023d235cf0f1144412d04c7e808e46e3df
Ebuube <onwutaebubegideon1555@gmail.com>
2024-01-18 18:25:44 +0000

    Annotated return value

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -7,6 +7,6 @@ import typing
 wait_random: typing.Callable = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> typing.Any:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """Return an asyncio task"""
     return asyncio.create_task(wait_random(max_delay))
