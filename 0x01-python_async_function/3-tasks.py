a791074ae37c232243c55a6871310b286e8f89a3
Ebube Gideon Onwuta <onwutaebubegideon1555@gmail.com>
2024-01-18 19:21:38 +0100

    Task 3: Tasks

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Tasks"""
+import asyncio
+import typing
+
+
+wait_random: typing.Callable = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> any:
+    """Return an asyncio task"""
+    return asyncio.create_task(wait_random(max_delay))
