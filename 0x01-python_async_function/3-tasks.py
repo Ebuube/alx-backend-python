8e1aec0965c581b775d360bb7d2951a6b753b780
Ebuube <onwutaebubegideon1555@gmail.com>
2024-01-18 18:24:08 +0000

    Corrected any type

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -7,6 +7,6 @@ import typing
 wait_random: typing.Callable = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> any:
+def task_wait_random(max_delay: int) -> typing.Any:
     """Return an asyncio task"""
     return asyncio.create_task(wait_random(max_delay))
